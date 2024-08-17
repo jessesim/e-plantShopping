30773e51a3120ccbba160bc85e21e2771bc06936
Jesse <jessesimmons12@yahoo.com>
2024-08-17 18:30:07 -0400

    merge from main

---
+++ src/CartSlice.jsx
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+    const plant = action.payload;
+    const existingItem = state.items.find(item => item.name === plant.name);
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      state.items.push({ ...plant, quantity: 1});
+    }
+    },
+    removeItem: (state, action) => {
+      const name = action.payload;
+      state.items = state.items.filter(item => item.name !== name);
+    },
+    updateQuantity: (state, action) => {
+    const {name, quantity} = action.payload;
+    const item = state.items.find(item => item.name === name);
+    if (item) {
+      item.quantity = quantity;
+    }
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
